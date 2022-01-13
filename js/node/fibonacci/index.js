d06899277fd0714eb44f94d1744dfd197579a9aa
jaytoven527 <jjnorwood527@gmail.com>
2022-01-13 17:41:58 -0600

    commit

--- js/node/fibonacci/index.js
+++ js/node/fibonacci/index.js
@@ -1,54 +1,59 @@
-
-
-
-function fibonacci_loop (num) {
-    
-    var numA, numB;
-        numA = 1;
-        numB = 0;
-        
-    var outputNum;
-    let stopTracker = num;
-    while(stopTracker > 0) {
-        var previousNum, nextNum;
-        
-        nextNum     = numA + numB;
-        
-        previousNum = numA;
-        numB        = previousNum;
-        numA        = nextNum;
-        outputNum   = numA;
-              
-        stopTracker--;
+   
+function fibonacci_recursive (num) {
+    if ( num <= 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2);
     }
-    return outputNum;
+}
+         
+const button = document.getElementById('button');
+var listNumbers = ''
+
+const state = 
+{
+    inputNumber: 0,
+    msg: null
+}
+button.addEventListener('click', function (e) { 
+    const number = document.getElementById('number');
+    const answer = fibonacci_recursive(number.value);
+    if(answer === 1) {   
+        state.msg = `Result: ${'poo poo pee pee'}`;
+    }
+    result.innerText = state.msg;
+});
+
+
+function handleUnder0(numberValue){
+    listNumbers += fibonacci_recursive(numberValue);
 }
 
+function answer (numberValue) {
+    let count = 1;
+    
+    if(numberValue <= 0) {
+        handleUnder0(listNumbers)
+    } else if(numberValue == 1) {
+        listNumbers += fibonacci_recursive(numberValue);
+    } else {
+        while(count <= numberValue) {
+            listNumbers += fibonacci_recursive(count) + ',' + ' ';
+        }
+    }
+    
+    return log(listNumbers);
+} 
 
-    var input, submit, result, msg, i, inputValue;
-    i = 1;
-    msg = '';
-    input = document.getElementById('input');
-    submit = document.getElementById('submit');
-    result = '';
-
-    function resultReturned () {
-     inputValue = input.value;
-         while (i <= inputValue) {
-             msg += fibonacci_loop(i) + ',' + ' ';
-             i++;
-             }
-        
-            
-    var el = document.getElementById('result');
-    result = 'Result: ' + msg;
-    el.innerHTML = result;
-    console.log(msg)
-               
+function writeMessage(res) {
+    return 'Result: ' + res;
 }
 
-
-
-
-
+function log(listNumbers) {
+    state.msg        = writeMessage(listNumbers);
+    const result = document.getElementById('result');
+    result.innerText = state.msg;
+}
 
